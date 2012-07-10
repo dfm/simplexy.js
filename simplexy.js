1d9e84e75c35ab3404724c854465299c2dd0d9c3
Dan F-M <danfm@nyu.edu>
2012-07-10 13:47:58 +0200

    the centroiding is working and there's a dumb unit test

--- simplexy.js
+++ simplexy.js
@@ -15,12 +15,12 @@ window.simplexy = (function () {
         }
     }
 
-    A = $M([[1, 1, 1, 1, 1, 1, 1, 1, 1], X, Y, X2, XY, Y2]);
+    A = $M([[1, 1, 1, 1, 1, 1, 1, 1, 1], X, Y, X2, XY, Y2]).transpose();
     AT = A.transpose();
 
-    simplexy.ATAinvA = AT.x(A).inv().x(AT).transpose();
+    simplexy.ATAinvAT = AT.x(A).inv().x(AT);
 
-    simplexy.simplexy = function (img) {
+    simplexy.centroid = function (img) {
         var i, j, results, a, b, c, d, e, f, patch = [], xc, yc;
         if (img.length !== 3 && img[0].length !== 3)
             throw "patch's got to be 3x3";
@@ -29,8 +29,7 @@ window.simplexy = (function () {
                 patch.push(img[i][j]);
 
         patch = $V(patch);
-        results = simplexy.ATAinvA.multiply(patch);
-        console.log(results);
+        results = simplexy.ATAinvAT.multiply(patch);
 
         a = results.elements[0];
         b = results.elements[1];
@@ -42,7 +41,29 @@ window.simplexy = (function () {
         xc = (2.0 * c * d + e * b) / (e * e - 4 * f * d);
         yc = (2.0 * b * f + e * c) / (e * e - 4 * f * d);
 
-        return [xc, yc];
+        return [yc, xc];
+    };
+
+    simplexy._gen_patch = function (p0) {
+        var x, y, img = [];
+        for (x = -1; x <= 1; x++) {
+            img.push([]);
+            for (y = -1; y <= 1; y++) {
+                var r2, dx = x - p0[0], dy = y - p0[1];
+                r2 = dx * dx + dy * dy;
+                img[img.length - 1].push(Math.exp(-0.5 * r2 / 81.));
+            }
+        }
+        return img;
+    };
+
+    simplexy.test = function () {
+        var p0 = [0.1, -0.3], img, p1;
+        img = simplexy._gen_patch(p0);
+        p1 = simplexy.centroid(img);
+        d = [p1[0] - p0[0], p1[1] - p0[1]];
+        if (d[0] * d[0] + d[1] * d[1] > 1e-2)
+            throw "This centroiding suuuuucks.";
     };
 
     return simplexy;
